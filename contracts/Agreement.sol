685c3eb15f7c779c9b95819af4009dac34aa2ace
gala1234 <galadriel.calero@gig.com>
2019-02-14 14:59:11 +0100

    give name for variables and functions

--- contracts/Agreement.sol
+++ contracts/Agreement.sol
@@ -1,10 +1,66 @@
 pragma solidity ^0.4.25;
 
 contract Agreement{
+
+//    uint256 public paymentDay;
     
-    uint256 public paymentDay;
-    
-    constructor(uint256 _paymentDay) public{
-        paymentDay = _paymentDay;
+//    constructor(uint256 _paymentDay) public{
+//        paymentDay = _paymentDay;
+//    }
+
+// las fechas se trabajan en 
+    struct Project{
+        // Se define en AgreementFactory es el Id del proyecto
+        uint agreementId;
+        // Se define en AgreementFactory breve descripcion
+        string description; 
+        // Se define en AgreementFactory, fecha en que se crea el smartcontract, fechas en timestamp
+        uint startDate; 
+        // Seteado en toDeliverProject(), fecha en que se entrega el proyecto
+        uint deliverDate;
+        // Se define en AgreementFactory `maxDaysToDeliver` y la suma de `deliverDate` y  `maxDaysToDeliver` nos da este parámetro (la suma se hace en toDeliverProject() 
+        uint deliverLimitDate; 
+        // Se define `maxDaysToRejected` en AgreementFactory y la suma de `deliverDate` y  `maxDaysToRejected ` nos da este parámetro (la suma se hace en toDeliverProject()
+        uint rejectedLimitDate; 
+        // Se define en AgreementFactory el dinero a pagar por el trabajo
+        uint amount; 
     }
+
+    struct projectState{
+        // Seteado en toDeliverProject()
+        bool delivered;
+        // Seteado en toDeliverProject()
+        bool outOfDate; 
+        // Seteado en toDeliverProject()
+        bool rejected;
+        // Seteado en toDeliverProject()
+        bool paid; 
+    }
+
+    // Se define en AgreementFactory
+    address public recipient; 
+    // Se define en AgreementFactory es el wallet donde se blockea el dinero del cliente
+    address public blockedAccount; 
+
+
+    function toDeliverProject(uint _toDayDate) public {
+    // Se comprueba que 'toDayDate' = 'deliverDate' < 'deliverLimitDate'.
+    // Cuándo se envía el trabajo al cliente se setéan los valores para: 'deliverDate', 'delivered','deliverLimitDate' y 'rejectedLimitDate'
+    }
+
+    function toOutOfDate(uint _toDayDate) public {
+	// Por defecto será false, se tiene que llamar a esta función desde otro sitio cuando 
+    // 'toDayDate' = 'deliverLimitDate' y comprobar el estado de 'delivered'. Si 'delivered' = false -> outOfDate = true
+    }
+
+    function toRejected(uint _toDayDate) public {
+    // Por defecto será false, se tiene que llamar a esta función desde otro sitio cuando 
+	// 'toDayDate' = 'rejectedLimitDate'. En caso de que lo haya rechazado (esta información nos llega desde un oráculo) -> rejected === true
+    }
+
+    function toPayTheProject(uint _toDayDate) public payable {
+    // Se tiene que llamar a esta función desde otro sitio cuando 'toDayDate' = 'rejectedLimitDate'
+	// Si `delivered===true`, `outOfDate===false` `rejected===false`, se ejecuta la transacción con valor 'amount' de la cuenta `blockedAccount` a la cuenta `recipient` y se setea `paid===true` 
+    }
+
 }
